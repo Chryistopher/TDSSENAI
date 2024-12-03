61a09bfe8bf77295c5ddfb5cd89d2746220f2fc0
Noemia <noemiamyia@gmail.com>
2024-12-03 20:13:40 -0300

    mudanças na descrição do exercício

--- manoeljoaquim/src/Main.java
+++ manoeljoaquim/src/Main.java
@@ -5,15 +5,18 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /*
-O Sr. Manoel Joaquim expandiu seus negócios para além dos negócios de 1,99
- e agora possui uma loja de conveniências. Faça um programa que implemente
-  uma caixa registradora rudimentar. O programa deverá receber um número
-   desconhecido de valores referentes aos preços das mercadorias.
-    Um valor zero deve ser informado pelo operador para indicar o final da compra.
-     O programa deve então mostrar o total da compra e perguntar o valor em
-      dinheiro que o cliente forneceu, para então calcular e mostrar o valor do troco.
-       Após esta operação, o programa deverá voltar ao ponto inicial, para registrar
-        a próxima compra. A saída deve ser conforme o exemplo abaixo:
+O Sr. Manoel Joaquim, conhecido pelos seus negócios de 1,99, decidiu expandir suas operações e agora possui uma loja de conveniência. Para isso, ele precisa de um programa que funcione como uma caixa registradora rudimentar.
+
+Este programa deve permitir que o operador registre o valor de cada produto comprado por um cliente. O processo funciona da seguinte maneira:
+
+O programa receberá os preços das mercadorias, um por vez, permitindo que o operador insira quantos produtos forem necessários.
+O valor zero (0) será utilizado para indicar o final da compra.
+Após registrar todos os produtos, o programa deve:
+Calcular e exibir o total da compra.
+Solicitar ao operador o valor em dinheiro fornecido pelo cliente.
+Calcular e exibir o troco.
+Depois de concluir a operação, o programa deve reiniciar para registrar a próxima compra.
+A saída do programa deve seguir o formato abaixo (os valores são apenas exemplos):
 Lojas Tabajara
 Produto 1: R$ 2.20
 Produto 2: R$ 5.80
